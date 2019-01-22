c9e0895ba40e8838e31180676153bd864c0b69f4
Klein Florian <florian.klein@free.fr>
2019-01-22 15:36:25 +0100

    add field title and type

--- src/Field.php
+++ src/Field.php
@@ -5,21 +5,28 @@ namespace Hippiemedia;
 final class Field
 {
     public $name;
+    public $type;
+    public $required;
     public $value;
+    public $title;
 
-    public function __construct(string $name, bool $required, string $value = null)
+    public function __construct(string $name, string $type, bool $required, string $value = null, string $title = null)
     {
         $this->name = $name;
+        $this->type = $type;
         $this->required = $required;
         $this->value = $value;
+        $this->title = $title;
     }
 
     public static function whatever(array $data = [])
     {
         return new self(
             $data['name'] ?? 'name',
+            $data['type'] ?? 'text',
             $data['required'] ?? true,
-            $data['value'] ?? 'value'
+            $data['value'] ?? 'value',
+            $data['title'] ?? 'title'
         );
     }
 }
