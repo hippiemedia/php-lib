e91700521cfcc05edd62ab2b9acbb9202b2274aa
Klein Florian <florian.klein@free.fr>
2019-07-23 13:23:40 +0200

    do not lose endpoints when two paths are the same

--- src/Format/OpenApi3.php
+++ src/Format/OpenApi3.php
@@ -31,83 +31,87 @@ final class OpenApi3 implements Format
                 'version' => '0.0.1',
             ]],
             $this->defaults,
-            ['paths' => iterator_to_array((function() use($resource) {
-                /** @var Link $link */
-                foreach ($resource->links as $link) {
-                    $path = parse_url($link->href, PHP_URL_PATH);
-                    yield $path => [
-                        'get' => [
-                            'operationId' => $link->rel(),
-                            'summary' => $link->title,
-                            'description' => $link->description,
-                            'parameters' => f\map($link->fields, function($field) {
-                                return [
-                                    'name' => $field->name,
-                                    'in' => 'path',
-                                    'required' => true,
-                                    'schema' => ['type' => 'string'],
-                                ];
-                            }),
-                            'responses' => ['default' => ['description' => 'ok']],
-                            'tags' => $link->extra['tags'] ?? [],
-                        ]
-                    ];
-                }
-                /** @var Operation $operation */
-                foreach ($resource->operations as $operation) {
-                    $path = parse_url($operation->url, PHP_URL_PATH);
-                    yield $path => [
-                        strtolower($operation->method) => array_filter([
-                            'operationId' => $operation->rel,
-                            'summary' => $operation->title,
-                            'description' => $operation->description,
-                            'parameters' => f\map($operation->urlFields, function($field) {
-                                return [
-                                    'name' => $field->name,
-                                    'in' => 'path',
-                                    'required' => true,
-                                    'schema' => ['type' => 'string'],
-                                ];
-                            }),
-                            'requestBody' => strtolower($operation->method) === 'delete' ? null : [
-                                'content' => array_filter(array_merge([
-                                    'application/x-www-form-urlencoded' => [
-                                        'schema' => array_filter([
-                                            'type' => 'object',
-                                            'properties' => iterator_to_array((function() use($operation) {
-                                                foreach ($operation->fields as $field) {
-                                                    yield $field['name'] => [
-                                                        'type' => 'string',
-                                                    ];
-                                                }
-                                            })()),
-                                        ]),
-                                    ]],
-                                    array_reduce([$operation], function($carry, $operation) {
-                                        $link = current(array_filter($operation->links, function($link) {
-                                            return $link->rel == ['request-schema'];
-                                        }));
-                                        if ($link) {
-                                            $carry = [
-                                                'application/json' => [
-                                                    'schema' => [
-                                                        '$ref' => $link->href,
+            ['paths' => array_merge_recursive(
+                iterator_to_array((function() use($resource) {
+                    /** @var Link $link */
+                    foreach ($resource->links as $link) {
+                        $path = parse_url($link->href, PHP_URL_PATH);
+                        yield $path => [
+                            'get' => [
+                                'operationId' => $link->rel(),
+                                'summary' => $link->title,
+                                'description' => $link->description,
+                                'parameters' => f\map($link->fields, function($field) {
+                                    return [
+                                        'name' => $field->name,
+                                        'in' => 'path',
+                                        'required' => true,
+                                        'schema' => ['type' => 'string'],
+                                    ];
+                                }),
+                                'responses' => ['default' => ['description' => 'ok']],
+                                'tags' => $link->extra['tags'] ?? [],
+                            ]
+                        ];
+                    }
+                })()),
+                iterator_to_array((function() use($resource) {
+                    /** @var Operation $operation */
+                    foreach ($resource->operations as $operation) {
+                        $path = parse_url($operation->url, PHP_URL_PATH);
+                        yield $path => [
+                            strtolower($operation->method) => array_filter([
+                                'operationId' => $operation->rel,
+                                'summary' => $operation->title,
+                                'description' => $operation->description,
+                                'parameters' => f\map($operation->urlFields, function($field) {
+                                    return [
+                                        'name' => $field->name,
+                                        'in' => 'path',
+                                        'required' => true,
+                                        'schema' => ['type' => 'string'],
+                                    ];
+                                }),
+                                'requestBody' => strtolower($operation->method) === 'delete' ? null : [
+                                    'content' => array_filter(array_merge([
+                                        'application/x-www-form-urlencoded' => [
+                                            'schema' => array_filter([
+                                                'type' => 'object',
+                                                'properties' => iterator_to_array((function() use($operation) {
+                                                    foreach ($operation->fields as $field) {
+                                                        yield $field['name'] => [
+                                                            'type' => 'string',
+                                                        ];
+                                                    }
+                                                })()),
+                                            ]),
+                                        ]],
+                                        array_reduce([$operation], function($carry, $operation) {
+                                            $link = current(array_filter($operation->links, function($link) {
+                                                return $link->rel == ['request-schema'];
+                                            }));
+                                            if ($link) {
+                                                $carry = [
+                                                    'application/json' => [
+                                                        'schema' => [
+                                                            '$ref' => $link->href,
+                                                        ],
                                                     ],
-                                                ],
-                                            ];
-                                        }
-                                        return $carry;
-                                    }, []),
-                                )),
-                            ],
-                            'responses' => ['default' => [
-                                'description' => 'ok',
-                            ]],
-                            'tags' => $operation->extra['tags'] ?? [],
-                        ]),
-                    ];
-                }
-            })()),
-        ]);
+                                                ];
+                                            }
+                                            return $carry;
+                                        }, []),
+                                    )),
+                                ],
+                                'responses' => ['default' => [
+                                    'description' => 'ok',
+                                ]],
+                                'tags' => $operation->extra['tags'] ?? [],
+                            ]),
+                        ];
+                    }
+                })()),
+            )]
+        );
     }
 }
